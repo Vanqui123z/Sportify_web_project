a99e5355444e422fc443bf91da375b375d4314b8
NGOCPRO123 <phamlengoc123@gmail.com>
2025-09-13 00:57:32 +0700

    update_danhmuclive_bongda

--- src/main/java/duan/sportify/controller/HomeController.java
+++ src/main/java/duan/sportify/controller/HomeController.java
@@ -3,7 +3,6 @@ package duan.sportify.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import duan.sportify.dao.BookingDetailDAO;
 import duan.sportify.dao.EventDAO;
@@ -46,6 +44,9 @@ public class HomeController {
 		return "user/index";
 	}
 
-	
+	@GetMapping("live-football")
+	public String liveFootball() {
+		return "redirect:https://xoilaczzcz.tv/";
+	}
 	
 }
